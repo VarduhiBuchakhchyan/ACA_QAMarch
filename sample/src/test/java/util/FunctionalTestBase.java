db879f63718e71a39163c1c4903c9c7c67000714
VarduhiBuchakhchyan <varduhi@live.com>
2018-05-11 13:49:00 +0400

    pom.xml as well as the webdriver path are updated

--- sample/src/test/java/util/FunctionalTestBase.java
+++ sample/src/test/java/util/FunctionalTestBase.java
@@ -10,7 +10,7 @@ public class FunctionalTestBase {
 	public WebDriver driver ;
 	final String URL = "https://google.com";
 	String chromeDriverProp = "webdriver.chrome.driver";
-	String driverPath="C:\\Users\\hovsepyana\\Downloads\\chromedriver.exe";
+	String driverPath="/Users/apple/Downloads/chromedriver";
 	
 	@BeforeClass
 	public void setup() {
